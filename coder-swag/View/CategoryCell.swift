f6e0f224fbdc16e2d80a5fb3c0d9687a576ef028
DutchRascal <Andre@iMac-4.local>
2017-08-05 17:53:11 +0200

    lesson-01

---
+++ coder-swag/View/CategoryCell.swift
@@ -0,0 +1,19 @@
+//
+//  CategoryCell.swift
+//  coder-swag
+//
+//  Created by Andre Boevink on 05/08/2017.
+//  Copyright © 2017 Andre Boevink. All rights reserved.
+//
+
+import UIKit
+
+class CategoryCell: UITableViewCell {
+    
+    @IBOutlet weak var categoryImage: UIImageView!
+    @IBOutlet weak var categoryTitle: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+}
