0aa830ee58c4a0a091b0efc56c4860a4e35eb24a
DutchRascal <Andre@iMac-4.local>
2017-08-06 15:05:43 +0200

    lesson-03

---
+++ coder-swag/View/ProductCell.swift
@@ -0,0 +1,17 @@
+//
+//  ProductCell.swift
+//  coder-swag
+//
+//  Created by Andre Boevink on 06/08/2017.
+//  Copyright © 2017 Andre Boevink. All rights reserved.
+//
+
+import UIKit
+
+class ProductCell: UICollectionViewCell
+{
+    
+    @IBOutlet weak var productImage: UIImageView!
+    @IBOutlet weak var productTitle: UILabel!
+    @IBOutlet weak var productPrice: UILabel!
+}
